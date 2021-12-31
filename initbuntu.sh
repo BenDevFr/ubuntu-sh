96fa196e432eb748209f7bf62d3a79cc91bd569f
BenDevFr <kannasylum@gmail.com>
2021-12-31 12:08:12 +0100

    Create initbuntu.sh

---
+++ initbuntu.sh
@@ -0,0 +1,91 @@
+sudo apt update
+sudo apt upgrade
+sudo wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb
+sudo apt install ./code_1.63.2-1639562499_amd64.deb
+sudo rm code_1.63.2-1639562499_amd64.deb
+
+#! Modification du bckgrd
+cd /usr/share/backgrounds/
+sudo wget https://i.imgur.com/ZWItHKf.jpeg
+sudo mv ZWItHKf.jpeg honeycomb.jpeg
+gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/honeycomb.jpeg
+
+
+#Creation des alias
+{
+echo "#Faire  \" .. \" au lieu de cd .."
+echo "alias ..='cd ..'"
+echo ""
+echo "#pour ouvrir le dossier html en raccourci tape www"
+echo "alias www='cd /var/www/html/'"
+echo ""
+echo "#gs pour git status"
+echo "alias gs='git status'"
+echo ""
+echo "#ça c'est une connerie"
+echo "alias gps=\"echo 'Vous etes ici -->*<--'\""
+echo ""
+echo "#reload-term pour le reload du term"
+echo "alias reload-term='. ~/.bashrc'"
+echo ""
+echo "#pour ouvrir le fichier de perso du term"
+echo "alias edit-term='code ~/.bash_aliases'"
+echo ""
+echo "#afficher la liste des alias ci-dessus"
+echo "alias alias-list='echo .. = cd .."
+echo "echo www = cd /var/www/html/"
+echo "echo gs = git status"
+echo "echo reload-term = . ~/.bashrc"
+echo "echo edit-term = code ~/.bash_aliases'"
+} >blork
+#
+
+#Suppression des jeux et appli useless
+sudo mv blork ~/.bash_aliases
+
+sudo apt remove  gnome-mahjongg -y
+#
+sudo apt remove  thunderbird -y
+#
+sudo apt remove  gnome-mines -y
+#
+sudo apt remove  aisleriot -y
+#
+sudo apt remove  gnome-sudoku -y
+
+
+#Install tweak tool
+#sudo apt install gnome-tweaks -y
+#Aller installer ça : https://extensions.gnome.org/extension/1160/dash-to-panel/
+
+#Install Chrome
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+#Install VSCode
+sudo wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb
+sudo dpkg -i code_1.63.2-1639562499_amd64.deb
+sudo rm code_1.63.2-1639562499_amd64.deb
+
+
+#Install apache2 + php
+#https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-20-04
+sudo apt-get install apache2 -y
+sudo apt install php libapache2-mod-php php-mysql -y
+
+#Install Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+
+#install MySQL
+sudo apt install mysql-server
+
+
+sudo apt autoclean
+sudo apt autoremove
